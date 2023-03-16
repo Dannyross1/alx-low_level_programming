99a3688df4e901a4f76b4f57ed97c93678a3c2a3
Dannyross1 <ukohdaniel@gmail.com>
2023-03-16 13:07:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
- * main - A program that prints a line using the printf function
- * return: 0 (success)
+ * main - A program that prints a line
+ * Return:0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
