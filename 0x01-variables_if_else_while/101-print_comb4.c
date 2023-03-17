57350174f4158762a7e58967ae1dd9dc95965e01
Dannyross1 <ukohdaniel@gmail.com>
2023-03-17 15:09:48 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ int main(void)
 					putchar (m);
 					putchar (n);
 					putchar (o);
-					if (n != 55 || o != 56)
+					if (n != 55 || != 56)
 					{
 						putchar (',');
 						putchar (' ');
